1d55769945e61aa6824285e1f298e073d6a56c10
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-13 09:11:42 -0400

    commit tree 0f86875a813878c1108433dfeea0c14c695c0afc

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    second_element = array[1]
+    third_element = array[2]
+    array[1] = third_element
+    array[2] = second_element
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |element|
+        element[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |element|
+        element[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+    array.map do |element|
+        if array[1] == element
+            element
+        else
+            element + "s"
+        end
+    end
+end
